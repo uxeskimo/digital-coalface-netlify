d94d8964b9d9f15279b265c2b65e943d03991ed6
Lee Murray <lee.murray@coopdigital.co.uk>
2018-08-09 18:12:38 +0100

    Removed asetts folder

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,13 +31,6 @@ module.exports = {
         path: `${__dirname}/src/images`
       }
     },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "img",
-        path: `${__dirname}/static/assets`
-      }
-    },
     "gatsby-transformer-sharp",
     "gatsby-plugin-sharp",
     "gatsby-transformer-remark"
