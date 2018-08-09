c902bfac3fbd6be23e383ab7a2d96726c1c769d4
Lee Murray <lee.murray@coopdigital.co.uk>
2018-08-09 17:57:04 +0100

    Added dir for static images

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,6 +31,13 @@ module.exports = {
         path: `${__dirname}/src/images`
       }
     },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "img",
+        path: `${__dirname}/static/assets`
+      }
+    },
     "gatsby-transformer-sharp",
     "gatsby-plugin-sharp",
     "gatsby-transformer-remark"
