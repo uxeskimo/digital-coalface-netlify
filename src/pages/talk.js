0958e8af58091310f1aa4d912bb25589bccd0f07
Lee Murray <lee.murray@coopdigital.co.uk>
2018-08-09 19:22:01 +0100

    Added content to form

--- src/pages/talk.js
+++ src/pages/talk.js
@@ -20,16 +20,20 @@ const About = ({ data }) => (
 
             <p>Please fill in the form to get in touch, alternatively send me an emai to uxeskimo@gmail.com</p>
 
-              <form
-                name="contact"
-                method="post"
-                data-netlify="true"
-                data-netlify-honeypot="bot-field"
-               >
+            <form
+              name="talk-form"
+              method="post"
+              data-netlify="true"
+              data-netlify-honeypot="bot-field" >
+              <label for="full-name">Full name</label>
+              <input type="text" name="full-name" id="full-name"/>
+              <label forfull-name="" className="">Email</label>
+              <input name="email" type="email" />
+              <label for="message">What would you like to talk about?</label>
+              <textarea name="message" />
+              <button className="btn">Send</button>
+            </form>
 
-                <input name ="name" type="text"/>
-                <button>Send</button>
-              </form>
 
             </div>
 
