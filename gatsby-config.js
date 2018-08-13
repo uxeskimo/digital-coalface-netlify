5c90f2239a07e4c9d0013bf3a5708f042881307d
Lee Murray <lee.murray@coopdigital.co.uk>
2018-08-13 07:38:54 +0100

    Added ga support

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,7 +44,22 @@ module.exports = {
               related: false, //Optional: Will remove related videos from the end of an embedded YouTube video.
               noIframeBorder: true //Optional: Disable insertion of <style> border: 0
             }
-    }
+    },
+
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-123761964-1",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+      },
+    },
 
   ]
 };
