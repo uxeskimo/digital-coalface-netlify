c47677917869c663cca0a286168373ad782e0d75
Lee Murray <lee.murray@coopdigital.co.uk>
2018-08-30 08:57:25 +0100

    Added fav icon

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import Helmet from "react-helmet";
 
 import DefaultHeader from "../components/Header";
+import FavIcon from "../images/favicon-32x32.png";
 import "./main.scss";
 
 const PageWrapper = ({ children, data, location }) => (
@@ -10,7 +11,11 @@ const PageWrapper = ({ children, data, location }) => (
     <Helmet title="The Digital Coalface"
             meta={[ { name: "description", content: "The digital coalface provides videos and podcasts for interacton designers, researchers, developers and product people to help them become better in their work." },
                     { name: "keywords", content: "Digital coalface, Lee Murray, podcast, videocast, interacton designer, UX, visual designer, researcher, content design, front-end developer, product owner." }
-    ]}/>
+    ]}
+    link={[
+      { rel: 'shortcut icon', type: 'image/png', href: `${FavIcon}` }
+      ]}
+    />
 
     {children()}
 
